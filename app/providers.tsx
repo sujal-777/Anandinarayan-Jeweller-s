ca33b45181cad0430c35293ea0729768db327c72
Asad <asad200ana@gmail.com>
2024-03-18 13:27:03 +0530

    UI libraries - material, nextui, aceternity, material icons, react icons

---
+++ app/providers.tsx
@@ -0,0 +1,19 @@
+"use client";
+
+import * as React from "react";
+// 1. import `NextUIProvider` component
+import { NextUIProvider } from "@nextui-org/react";
+import { AppRouterCacheProvider } from "@mui/material-nextjs/v13-appRouter";
+import { ThemeProvider } from "@mui/material/styles";
+import theme from "./theme";
+
+// 2. Wrap NextUIProvider at the root of your app
+export default function Providers({ children }: { children: React.ReactNode }) {
+  return (
+    <AppRouterCacheProvider>
+      <ThemeProvider theme={theme}>
+        <NextUIProvider>{children}</NextUIProvider>
+      </ThemeProvider>
+    </AppRouterCacheProvider>
+  );
+}
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,10 +1,15 @@
 import type { Config } from "tailwindcss";
+const { nextui } = require("@nextui-org/react");
+const {
+  default: flattenColorPalette,
+} = require("tailwindcss/lib/util/flattenColorPalette");
 
 const config: Config = {
   content: [
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
     extend: {
@@ -15,6 +20,19 @@ const config: Config = {
       },
     },
   },
-  plugins: [],
+  darkMode: "class",
+  plugins: [nextui(), addVariablesForColors, require("tailwindcss-animate")],
 };
+
+function addVariablesForColors({ addBase, theme }: any) {
+  let allColors = flattenColorPalette(theme("colors"));
+  let newVars = Object.fromEntries(
+    Object.entries(allColors).map(([key, val]) => [`--${key}`, val])
+  );
+
+  addBase({
+    ":root": newVars,
+  });
+}
+
 export default config;
