179d15141efccc79a059d27509c8faf420f0bf9b
Asad <asad200ana@gmail.com>
2024-03-20 14:43:41 +0530

    add: navbar link for rooms and suits

--- app/_components/NavBar/Nav.tsx
+++ app/_components/NavBar/Nav.tsx
@@ -47,7 +47,7 @@ export default function Nav() {
               </li>
               <li>
                 <a
-                  href="#"
+                  href="/rooms_and_suits"
                   className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
                 >
                   ROOM & SUITES
