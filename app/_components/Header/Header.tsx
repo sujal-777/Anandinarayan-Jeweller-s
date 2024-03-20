d46a299559769a051e280260e55f3a37e8a39caf
Asad <asad200ana@gmail.com>
2024-03-20 14:09:53 +0530

    add + fix: homepage fixed and added home for rooms and suits page custom components

--- app/_components/Header/Header.tsx
+++ app/_components/Header/Header.tsx
@@ -1,24 +1,32 @@
 // Header.tsx
-'use cleint'
+"use cleint";
 
-import React from 'react';
+import React from "react";
 
 interface HeaderProps {
-    aboutUsText: string;
-    headerBannerSrc: string;
+  aboutUsText: string;
+  headerBannerSrc: string;
 }
 
 export default function Header({ aboutUsText, headerBannerSrc }: HeaderProps) {
-    return (
-        <section className="relative">
-            <img src={headerBannerSrc} alt="Header" className="w-full h-96 object-cover" />
-            <div className="absolute top-5 left-5 flex flex-col items-start w-full h-full">
-                <img src="/HeaderAssets/hotel_logo.png" alt="Hotel Logo" className="w-20 md:w-40" />
-                <div className="absolute top-40 left-5 flex flex-col items-start w-full h-full">
-                    <p className="text-white">LUXURY HOTEL</p>
-                    <p className="text-white text-8xl md:text-4xl">{aboutUsText}</p>
-                </div>
-            </div>
-        </section>
-    );
+  return (
+    <section className="relative">
+      <img
+        src={headerBannerSrc}
+        alt="Header"
+        className="w-full h-96 object-cover"
+      />
+      <div className="absolute top-5 left-5 flex flex-col items-start w-full h-full">
+        <img
+          src="/HeaderAssets/hotel_logo.png"
+          alt="Hotel Logo"
+          className="w-20 md:w-40"
+        />
+        <div className="absolute top-40 left-5 flex flex-col items-start w-full h-full">
+          <p className="text-white">LUXURY HOTEL</p>
+          <p className="text-white text-8xl md:text-4xl">{aboutUsText}</p>
+        </div>
+      </div>
+    </section>
+  );
 }
--- app/rooms_and_suits/page.tsx
+++ app/rooms_and_suits/page.tsx
@@ -1,5 +1,17 @@
 import React from "react";
 
+import Hero from "./hero";
+// import Page from "./roomdisplay";
+import Search from "./searchrooms";
+import Room from "./roomDisplayNew";
+
 export default function rooms() {
-  return <section>Hello!</section>;
+  return (
+    <section>
+      <Hero />
+      <Room />
+      {/* <Page /> */}
+      <Search />
+    </section>
+  );
 }
