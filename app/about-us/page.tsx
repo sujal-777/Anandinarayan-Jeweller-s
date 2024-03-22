86901106f7ecb4f29d2955afc62b2ee23a645f52
Asad <asad200ana@gmail.com>
2024-03-22 14:16:40 +0530

    feature(about-us): director

--- app/about-us/page.tsx
+++ app/about-us/page.tsx
@@ -2,12 +2,14 @@ import React from "react";
 
 import Hero from "./hero";
 import Industry from "./industry";
+import Director from "./director";
 
 const page = () => {
   return (
     <div>
       <Hero />
       <Industry />
+      <Director />
     </div>
   );
 };
