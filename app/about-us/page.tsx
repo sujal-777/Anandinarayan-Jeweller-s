67e92be5789b3e71bc07204692fd36c2cf2c4b6d
Asad <asad200ana@gmail.com>
2024-03-22 13:21:16 +0530

    feature(about-us): hero, intro

---
+++ app/about-us/page.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+import Hero from "./hero";
+import Industry from "./industry";
+
+const page = () => {
+  return (
+    <div>
+      <Hero />
+      <Industry />
+    </div>
+  );
+};
+
+export default page;
