91634366ef20b333da8b038629e3614a83cd8f46
Asad <asad200ana@gmail.com>
2024-04-08 06:42:47 +0530

    foods template

---
+++ app/news/foods/page.tsx
@@ -0,0 +1,17 @@
+import Hero from "./hero";
+import Struct from "./Struct";
+
+export default function page() {
+  return (
+    <>
+      <Hero />
+      <div className="flex flex-col md:flex-row items-stretch justify-center min-h-screen px-4">
+        <div className="hidden flex-grow lg:block md:w-1/12 lg:w-1/6 bg-[url('../public/short_border.svg')] bg-repeat bg-contain bg-center"></div>
+
+        <Struct />
+
+        <div className="hidden flex-grow lg:block md:w-1/12 lg:w-1/6 bg-[url('../public/short_border.svg')] bg-repeat bg-contain bg-center"></div>
+      </div>
+    </>
+  );
+}
